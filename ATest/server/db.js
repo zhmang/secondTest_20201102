559aa0fe705b8637d1fed541043a9492cf89ae3a
zhengmanrong <zhengmanrong@do1.com.cn>
2020-11-02 17:45:07 +0800

    feat:main文件

---
+++ ATest/server/db.js
@@ -0,0 +1,10 @@
+// 数据库连接配置db.js
+module.exports = {
+  mysql: {
+    host: '47.99.153.***',//mysql连接ip地址
+    user: 'abc',
+    password: 'abc123',//mySql用户名密码
+    database: 'abc_ddd',//mySql数据库名
+    port: '3306'//mysql连接端口
+  }
+}
