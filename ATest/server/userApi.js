63b0d322537e9f1362cac7f3471d639acaa4d733
zhengmanrong <zhengmanrong@do1.com.cn>
2020-11-03 10:12:52 +0800

    Revert "test:test undo commit"
    This reverts commit b6076bce

--- ATest/server/userApi.js
+++ ATest/server/userApi.js
@@ -3,7 +3,6 @@ var express = require('express')
 var router = express.Router()
 var mysql = require('mysql')
 var $sql = require('../sqlMap')
-console.log(111)
 
 // 连接数据库
 var conn = mysql.createConnection(models.mysql)
