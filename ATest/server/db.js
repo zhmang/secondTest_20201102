4a8cb67603ceba669965316d13c342d7e89edf3a
zhengmanrong <zhengmanrong@do1.com.cn>
2020-11-03 10:23:54 +0800

    fix:修改db文件

--- ATest/server/db.js
+++ ATest/server/db.js
@@ -1,7 +1,7 @@
 // 数据库连接配置db.js
 module.exports = {
   mysql: {
-    host: '47.99.153.***',//mysql连接ip地址
+    host: '47.99.153.111',//mysql连接ip地址
     user: 'abc',
     password: 'abc123',//mySql用户名密码
     database: 'abc_ddd',//mySql数据库名
