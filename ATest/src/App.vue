7db6c66ea76daafc9a63292cfca4580e0391e12d
zhmang <73813469+zhmang@users.noreply.github.com>
2020-11-03 11:15:05 +0800

    Update App.vue

--- ATest/src/App.vue
+++ ATest/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
-    <el-form-item label="姓名" prop="name">
+    <el-form-item label="名字" prop="name">
       <el-input v-model="ruleForm.name"></el-input>
     </el-form-item>
     <el-form-item label="班级" prop="class">
